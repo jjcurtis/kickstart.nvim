aa39549664c36b9bdefb69e8a3f41032893a49a1
Jonathan Curtis <jonathan.j.curtis@outlook.com>
2025-08-26 22:29:04 -0600

    Add new keymap for NeoGit

--- lua/configs/keymaps.lua
+++ lua/configs/keymaps.lua
@@ -8,6 +8,11 @@ vim.keymap.set('n', '<leader>n', function()
   vim.cmd 'Neotree toggle'
 end, { desc = 'Toggle [N]eotree' })
 
+-- Open NeoGit
+vim.keymap.set('n', '<leader>g', function()
+  vim.cmd 'Neogit'
+end, { desc = 'Neo[G]it' })
+
 -- Exit terminal mode in the builtin terminal with a shortcut that is a bit easier
 -- for people to discover. Otherwise, you normally need to press <C-\><C-n>, which
 -- is not what someone will guess without a bit more experience.
