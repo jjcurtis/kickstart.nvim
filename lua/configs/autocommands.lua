6a848d8c2d31d8940051c9666685646393c10280
Jonathan Curtis <jonathan.j.curtis@outlook.com>
2025-08-29 23:23:22 -0600

    Add Nvim-Dap-Ui plugin and autocommand for attachment

--- lua/configs/autocommands.lua
+++ lua/configs/autocommands.lua
@@ -15,3 +15,24 @@ vim.api.nvim_create_autocmd('VimEnter', {
     end
   end,
 })
+
+vim.api.nvim_create_autocmd('VimEnter', {
+  desc = 'Setup DAP-UI',
+  group = vim.api.nvim_create_augroup('dapui-setup', { clear = true }),
+  callback = function()
+    local dap, dapui = require 'dap', require 'dapui'
+    dapui.setup()
+    dap.listeners.before.attach.dapui_config = function()
+      dapui.open()
+    end
+    dap.listeners.before.launch.dapui_config = function()
+      dapui.open()
+    end
+    dap.listeners.before.event_terminated.dapui_config = function()
+      dapui.close()
+    end
+    dap.listeners.before.event_exited.dapui_config = function()
+      dapui.close()
+    end
+  end,
+})
